e204224f36f14ac2e4a9e94304f8907a9595dc40
Chloe Tan <chloe.tan95@gmail.com>
2021-01-30 14:10:18 -0500

    Complete returning column headers in csv report

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -7,16 +7,38 @@ app.use(express.urlencoded({
   extended: true
 }))
 
-app.get('/submit-form', (req, res) => {
+app.use(express.json());
+
+app.get('/', (req, res) => {
   console.log('connected to server!');
 
   res.sendFile(path.join(__dirname + '/client/index.html'));
 })
 
 app.post('/submit-form', (req, res) => {
-  console.log('submit req: ', req.body);
-  //what the client inputted into form is the req.body.text
-  //res.end();
+  var reqJSON = JSON.parse(req.body.text);
+  var columnHeaders = Object.keys(reqJSON);
+  columnHeaders.pop();
+  var csvReport = columnHeaders.toString();
+
+
+  res.send(csvReport);
+
+  //recursive function to loop through json
+  // var convertToCSV = function (item) {
+  //   if (item.children.length === 0) {
+  //     return;
+  //   }
+
+  //   for (var i = 0; i < item.children.length; i++) {
+  //     convertToCSV(item.children[i]);
+  //   }
+  // }
+
+  // convertToCSV(reqJSON);
+
+
+  //res.sendFile(path.join(__dirname + '/client/index.html'));
 })
 
 app.listen(port, () => {
