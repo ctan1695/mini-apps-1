effaeabe94764b2297f75805026adc657f25d785
Chloe Tan <chloe.tan95@gmail.com>
2021-01-30 11:45:26 -0500

    Complete using default browser behavior to POST from client to server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,24 +1,22 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const port = 5500;
 
+app.use(express.urlencoded({
+  extended: true
+}))
 
-/*
-- event listener on submit button click
-  --> trigger POST request
-*/
-
-
-app.get('/', (req, res) => {
+app.get('/submit-form', (req, res) => {
   console.log('connected to server!');
-  //console.log('req: ', req);
+
   res.sendFile(path.join(__dirname + '/client/index.html'));
 })
 
-app.post('/submit', (req, res) => {
+app.post('/submit-form', (req, res) => {
   console.log('submit req: ', req.body);
+  //what the client inputted into form is the req.body.text
+  //res.end();
 })
 
 app.listen(port, () => {
