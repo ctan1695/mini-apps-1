14f5d1291511d6bb4cc673690feccc377f9bbe49
Chloe Tan <chloe.tan95@gmail.com>
2021-02-14 14:28:56 -0500

    Set up babel and served up html file

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -10,3 +10,14 @@
 
 - POST router
 */
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Listing on port: ${port}`);
+})
