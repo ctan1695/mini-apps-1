344deffcbeed4cce439afaf435b76abeb73773b5
Chloe Tan <chloe.tan95@gmail.com>
2021-02-15 17:16:32 -0500

    Undid everything to start over

--- challenge_3/client/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-const element = document.querySelector('#app');
-
-console.log('element: ', element);
--- challenge_3/database.js
+++
@@ -1,2 +0,0 @@
-// https://sequelize.org/master/manual/getting-started.html
-
