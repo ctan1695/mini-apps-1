52eebc08bf7f2c7f9ec37584041202fce1909bab
Chloe Tan <chloe.tan95@gmail.com>
2021-02-16 00:57:20 -0500

    Pseudocode my plan for AJAX calls from client

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -10,6 +10,7 @@ class App extends React.Component {
 
     this.state = {
       currentPage: Home
+      userID: 0
     }
 
     this.handleCheckout = this.handleCheckout.bind(this);
@@ -20,19 +21,26 @@ class App extends React.Component {
   }
 
   handleCheckout() {
-    console.log('checkout clicked');
+    //AJAX request to POST new record into db (INSERT into customers table with just the primary key and Name = empty string)
+    //Make the DB and server return the Primary Key value to the client.
+    //Set state for UserID to be the ID received from server response.
     this.setState({currentPage: Name});
   }
 
   handleNextForName() {
+    //AJAX request to POST new record into db (UPDATE customers table: Name, Email and Password columns with user inputted info WHERE ID = this.state.userID)
     this.setState({currentPage: Address});
   }
 
   handleNextForAddress() {
+    //AJAX request to POST new record into db (INSERT into addresses table: non-FK columns should be the info from the customer's input and customer_ID should be this.state.userID)
     this.setState({currentPage: Payment});
   }
 
   handleNextForPayment() {
+    //AJAX request to POST new record into db (INSERT into payment table: non-FK columns should be the info from the customer's input and customer_ID should be this.state.userID)
+    //AJAX request to GET all information tied to this.state.userID.
+    //Set state using the info received on server response (should be the name, address and payment info for the user).
     this.setState({currentPage: Confirmation});
   }
 
