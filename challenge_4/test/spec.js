2137fdec6eb88ca84238050a68ba5ea11fae5767
Chloe Tan <chloe.tan95@gmail.com>
2021-02-27 22:01:54 -0500

    Complete creating tests

--- challenge_4/test/spec.js
+++ challenge_4/test/spec.js
@@ -8,9 +8,126 @@
 
 */
 
-var assert = require('assert');
-describe('Array', function () {
-  it('should return -1 when the value is not present', function () {
-    assert.equal([1, 2, 3].indexOf(4), -1);
+var chai = require('chai');
+var assert = chai.assert;
+var expect = chai.expect;
+describe('Win Helper Functions', function () {
+  it('should detect a horizontal win', function () {
+    var board =
+      [ [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0, 0],
+        [1, 2, 2, 2, 2, 0, 0]];
+
+    var checkHorizontalWin = function (player) {
+      // var board = this.state.board;
+      var winner = false;
+
+      for (var r = 0 ; r < board.length; r++) {
+        if (!winner) {
+          var row = board[r];
+          if (row.includes(player)) {
+            for (var c = 0; c < row.length; c++) {
+              if (row[c] === player && row[c + 1] === player && row[c + 2] === player && row[c + 3] === player) {
+                winner = true;
+                return winner;
+              }
+            }
+          }
+        }
+      }
+      return winner;
+    }
+
+    assert.equal(checkHorizontalWin(2), true);
+    assert.equal(checkHorizontalWin(1), false);
+  })
+
+  it('should detect a vertical win', function () {
+    var board =
+      [ [0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0],
+        [0, 1, 2, 0, 0, 0, 0],
+        [0, 1, 2, 0, 0, 0, 0],
+        [0, 1, 1, 0, 0, 0, 0],
+        [1, 2, 2, 2, 0, 0, 0]];
+
+    var checkVerticalWin = function (player) {
+      // var board = this.state.board;
+      var winner = false;
+
+      for (var r = 3; r < board.length; r++) {
+        for (var c = 0; c < board[r].length; c++) {
+          if (board[r][c] === player &&
+              board[r - 1][c] === player &&
+              board[r - 2][c] === player &&
+              board[r - 3][c] === player) {
+                return true;
+          }
+        }
+      }
+      return false;
+    }
+
+    assert.equal(checkVerticalWin(1), true);
+    assert.equal(checkVerticalWin(2), false);
+  })
+
+  it('should detect a negative diagonal win', function () {
+    var board =
+      [ [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0],
+        [0, 1, 1, 2, 0, 0, 0],
+        [0, 2, 1, 1, 2, 0, 0],
+        [1, 2, 2, 2, 1, 0, 0]];
+
+    var checkNegativeDiagonalWin = function (player) {
+      // var board = this.state.board;
+      for (var r = 3; r < board.length; r++) {
+        for (var c = 3; c < 7; c++) {
+          if (board[r][c] === player &&
+              board[r - 1][c - 1] === player &&
+              board[r - 2][c - 2] === player &&
+              board[r - 3][c - 3] === player) {
+            return true;
+          }
+        }
+      }
+      return false;
+    }
+
+    assert.equal(checkNegativeDiagonalWin(1), true);
+    assert.equal(checkNegativeDiagonalWin(2), false);
+  })
+
+  it('should detect a positive diagonal win', function () {
+    var board =
+      [ [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 2, 0, 0],
+        [0, 0, 0, 2, 1, 0, 0],
+        [0, 1, 2, 1, 1, 0, 0],
+        [1, 2, 2, 2, 1, 0, 0]];
+
+    var checkPositiveDiagonalWin = function (player) {
+      // var board = this.state.board;
+      for (var r = 3; r < board.length; r++) {
+        for (var c = 0; c < 4; c++) {
+          if (board[r][c] === player &&
+              board[r - 1][c + 1] === player &&
+              board[r - 2][c + 2] === player &&
+              board[r - 3][c + 3] === player ) {
+            return true;
+          }
+        }
+      }
+      return false;
+    }
+
+    assert.equal(checkPositiveDiagonalWin(2), true);
+    assert.equal(checkPositiveDiagonalWin(1), false);
   })
 })
--- challenge_4/webpack.config.js
+++ challenge_4/webpack.config.js
@@ -13,7 +13,8 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['transform-export-extensions', '@babel/plugin-transform-react-jsx']
           }
         }
       }
